d1935c4aa43510034d218003f82db6d512104159
Lucas Santana <lucas.diassantana@gmail.com>
2026-02-24 17:40:38 -0300

    feat: add checkout page composition for ecommerce

--- src/lib/design-references/template-compositions/init.ts
+++ src/lib/design-references/template-compositions/init.ts
@@ -2,7 +2,7 @@ import { registerComposition } from './index.js';
 import { saasCentered, productLaunch, waitlistMinimal, agencyPortfolio } from './landing/index.js';
 import { analytics, crmPipeline, projectBoard } from './dashboard/index.js';
 import { login, signup, onboarding } from './auth/index.js';
-import { storefront, productDetail, cart } from './ecommerce/index.js';
+import { storefront, productDetail, cart, checkout } from './ecommerce/index.js';
 import { blogListing, blogPost } from './content/index.js';
 
 export function initializeCompositions(): void {
@@ -22,6 +22,7 @@ export function initializeCompositions(): void {
   registerComposition(storefront);
   registerComposition(productDetail);
   registerComposition(cart);
+  registerComposition(checkout);
 
   registerComposition(blogListing);
   registerComposition(blogPost);
